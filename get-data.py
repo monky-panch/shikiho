4383bdcb490c805ee2e887209560c95914d82bcb
monky-panch <555bee2@mail.bbexcite.jp>
2018-06-19 17:53:34 +0900

    urlヲらくてん

--- get-data.py
+++ get-data.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 target_url = 'http://yahoo.co.jp'
+target_url = 'https://member.rakuten-sec.co.jp/app/info_jp_quarterly.do;BV_SessionID=307855EFEDC233C794EE3FB980892E16.47a66bab?eventType=init&dscrCd=99480'
 htmldate = requests.get(target_url)
 try:
     soup = BeautifulSoup(htmldate.text,'lxml')
